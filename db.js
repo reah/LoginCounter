b3830b2bf42c3b458e130ec61c0ce5e2d9dc8ca2
reah <reahmiyara@berkeley.edu>
2014-10-01 04:07:18 -0700

    added mongolab uri to connect

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/users', function () {
+mongoose.connect(process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||'mongodb://127.0.0.1:27017/users', function () {
   console.log('mongodb connected')
 })
 module.exports = mongoose
